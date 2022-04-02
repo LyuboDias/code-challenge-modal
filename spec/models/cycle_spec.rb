a902410e053587f463e72d70539af026dece850d
Lyubomir Dias <lyubomirdias@gmail.com>
2022-04-02 19:07:05 +0100

    created spec againts the public? method, includes checks for public_status true and false

--- spec/models/cycle_spec.rb
+++ spec/models/cycle_spec.rb
@@ -5,14 +5,12 @@ RSpec.describe Cycle, type: :model do
 
   it { should validate_presence_of (:name) }
 
-  # it "returns cycle, if public_status: true" 
-  #   # cycle = build(:cycle, public_status: true)
-  #   expect(cycle).to be_public
-  # end
+  it "expected to return cycle, only if public_status: true" do
+    cycle = create(:cycle, public_status: true)
+    expect(cycle).to be_public
 
-  # it "returns cycle, if public_status: false" 
-  #   # cycle = build(:cycle, public_status: false)
-  #   expect(cycle).not_to be_public
-  # end
-  # TODO spec if method returns only cycles with public_status: true, so index method in controller will have only public cycles
+    cycle = create(:cycle, public_status: false)
+    expect(cycle).not_to be_public
+  end
 end
+# git add . && git commit -m "created spec againts the public? method, includes checks for public_status true and false"
