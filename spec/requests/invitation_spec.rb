fe3c07838021983af00dc252961596c5729b350f
Lyubomir Dias <lyubomirdias@gmail.com>
2022-04-05 20:45:31 +0100

    removed commented code

--- spec/requests/invitation_spec.rb
+++ spec/requests/invitation_spec.rb
@@ -1,26 +1,3 @@
-# require 'rails_helper'
-
-# RSpec.describe "Invitations", type: :request do
-#   describe 'POST create' do
-#     let!(:cycle) { create :cycle, name: 'lorem' }
-#     let!(:invitation) { create :invitation, email: 'test@gmail.com', cycle_id: cycle.id, association: cycle }
-
-#     it "opens form modal" do
-#       post :create
-#       expect(response).to have_http_status(:ok)
-#       expect(response.body).to include invitation.email
-#     end
-#   end
-
-#   describe "responds to" do
-#     it "responds to html by default" do
-#       post :create, :params => { :cycle => { :name => "lorem" }, :invitation => { :email => "test@gmail.com", cycle_id: cycle.id, :association => cycle } }
-#       expect(response.media_type).to eq "text/html"
-#     end
-#   end
-# end
-
-
 require 'rails_helper'
 
 RSpec.describe InvitationsController, type: :controller do
