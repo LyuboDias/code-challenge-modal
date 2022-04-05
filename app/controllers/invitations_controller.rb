1830823a156cdf19e43f2cd353657886a876608e
Lyubomir Dias <lyubomirdias@gmail.com>
2022-04-05 19:24:43 +0100

    created extract_name that we call before the create method

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,4 +1,5 @@
 class InvitationsController < ApplicationController
+    before_action :extract_name, only: [:create]
 
     def new
         @cycle = Cycle.find(params[:cycle_id])
@@ -10,7 +11,7 @@ class InvitationsController < ApplicationController
     end
 
     def create
-        @invitation = Invitation.new(invitation_params)
+        @invitation = Invitation.new(invitee: @name, message: invitation_params[:message], email: invitation_params[:email])
         @invitation.cycle_id = (params[:cycle_id])
         # if invitation is valid and saved -> redirect home
         if @invitation.save!
@@ -20,6 +21,11 @@ class InvitationsController < ApplicationController
             render :new
         end
     end
+
+    def extract_name
+        @name = invitation_params[:email].split('@')[0].gsub('.', ' ').titleize
+    end
+    
 
     private
     
