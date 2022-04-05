efcc28dfc4ec911070c3b03abb5341ddea9e7c6e
Lyubomir Dias <lyubomirdias@gmail.com>
2022-04-05 20:02:45 +0100

    added if statement to extract_name when string has '<', created extract_email method

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,5 @@
 class InvitationsController < ApplicationController
-    before_action :extract_name, only: [:create]
+    before_action :extract_name, :extract_email, only: [:create]
 
     def new
         @cycle = Cycle.find(params[:cycle_id])
@@ -11,7 +11,7 @@ class InvitationsController < ApplicationController
     end
 
     def create
-        @invitation = Invitation.new(invitee: @name, message: invitation_params[:message], email: invitation_params[:email])
+        @invitation = Invitation.new(invitee: @name, message: invitation_params[:message], email: @email)
         @invitation.cycle_id = (params[:cycle_id])
         # if invitation is valid and saved -> redirect home
         if @invitation.save!
@@ -23,9 +23,21 @@ class InvitationsController < ApplicationController
     end
 
     def extract_name
-        @name = invitation_params[:email].split('@')[0].gsub('.', ' ').titleize
+        if invitation_params[:email].include?('<')
+            # split on < and capitalize
+            @name = invitation_params[:email].split('<')[0].titleize
+        else
+            # split email at @ to get the name like so: lyubo.dias => swap . with ' ' white space and capitalize every word in the string
+            @name = invitation_params[:email].split('@')[0].gsub('.', ' ').titleize
+        end
     end
-    
+
+    def extract_email
+        if invitation_params[:email].include?('<')
+            # split on < and remove last char >
+            @email = invitation_params[:email].split('<')[1].chop
+        end
+    end
 
     private
     
